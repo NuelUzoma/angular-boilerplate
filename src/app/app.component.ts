a3877c06269dfe1d4531015b125c4d8853bab9c6
NuelUzoma <emcool17@gmail.com>
2024-08-07 13:40:55 +0100

    [home-page]: addedmore responsiveness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { HlmButtonDirective } from '../../components/ui-button-helm/src/lib/hlm-button.directive';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,32 @@ import { HlmButtonDirective } from '../../components/ui-button-helm/src/lib/hlm-
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {}
+export class AppComponent implements OnInit, OnDestroy {
+  /**
+   * Toggle menu function to encapsulate signup & login at 1024px
+   */
+  isMenuOpen = false;
+  private routerSubscription!: Subscription;
+
+  constructor(
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.routerSubscription = this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.isMenuOpen = false;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from "@angular/core";
+import { HlmButtonDirective } from "../../../../components/ui-button-helm/src/lib/hlm-button.directive";
+import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [],
+    imports: [RouterOutlet, RouterLink, RouterLinkActive, HlmButtonDirective],
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss']
 })
